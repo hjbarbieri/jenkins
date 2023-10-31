a8b8154d91d04163f0d3ad69df44766775f7837c
Javier Barbieri <jbarbieri@morean.co>
2023-10-31 14:05:49 -0300

    Create nodejs-DSL.groovy

---
+++ nodejs-DSL.groovy
@@ -0,0 +1,37 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/hjbarbieri/jenkins.git', 'master') { node ->
+            node / gitConfigName('hjbarbieri')
+            node / gitConfigEmail('jbarbieri@morean.co')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
